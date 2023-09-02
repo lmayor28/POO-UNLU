ac4487fa8a6a6851c2bbf341579702bc4e1cee28
Luis Mayor Moreno <lmayormoreno@gmail.com>
2023-09-02 14:27:44 -0300

    EJ5 Completado.

---
+++ src/TP1/EJ5/EjecutableEJ5.java
@@ -0,0 +1,31 @@
+package TP1.EJ5;
+
+import java.time.LocalDate;
+// Preguntar sobre como se maneja el completado en fechas anteriores, si usar un localDate especifico y no usar el LocalDate.now();
+public class EjecutableEJ5 {
+    public static void main(String[] args) {
+
+        // Primera tarea con el tiempo de vencimiento para mañana.
+        Tarea tarea1 = new Tarea("Ir al supermercado mañana", 1, LocalDate.now().plusDays(1));
+
+        // Segunda tarea con el tiempo de vencimiento para ayer.
+        Tarea tarea2 = new Tarea("Consultar repuesto del auto", 2, LocalDate.now().minusDays(1));
+
+        // Tercera tarea con el tiempo de vencimiento para hoy.
+        Tarea tarea3 = new Tarea("Ir al cine a ver la nueva película de Marvel", 3,  LocalDate.now().minusDays(1));
+
+        tarea1.mostrarTarea();
+        tarea2.mostrarTarea();
+        tarea3.mostrarTarea();
+
+        System.out.println("\nSe completan todas las tareas.... \n");
+        tarea1.completarTarea();
+        tarea2.completarTarea();
+        tarea3.completarTarea();
+
+        tarea1.mostrarTarea();
+        tarea2.mostrarTarea();
+        tarea3.mostrarTarea();
+
+    }
+}
--- src/TP1/EJ5/Tarea.java
+++ src/TP1/EJ5/Tarea.java
@@ -2,35 +2,82 @@ package TP1.EJ5;
 import java.time.LocalDate;
 
 
+
 public class Tarea {
+
+    public enum Estado {
+        incompleta,
+        completa,
+        vencida
+    }
     private String descripcion;
     private int prioridad;
-    private boolean estado = false;
+    private Estado estado;
     private LocalDate fechaLimite;
+    private LocalDate fechaCompletada;
 
+//    Constructor:
+    public Tarea(String descripcion, int prioridad){
+        this(descripcion, prioridad, null);
+    }
+    public Tarea(String descripcion, int prioridad, LocalDate fechaLimite){
+        this.descripcion = descripcion;
+        this.prioridad = prioridad;
+        this.fechaLimite = fechaLimite;
+
+        // Comprobamos si la tarea es vencida o no y le asignamos su estado correspondiente.
+        if (fechaLimite == null){
+            this.estado = Estado.incompleta;
+            return;
+        }
+        if (fechaLimite.isBefore(LocalDate.now())){
+            this.estado = Estado.vencida;
+        } else {
+            this.estado = Estado.incompleta;
+        }
+    }
+
+//    Setter y getter:
     public void setDescripcion(String descripcion) {
         this.descripcion = descripcion;
     }
     public String getDescripcion() {
         return descripcion;
-        //LocalDate date = new LocalDate(2012, 12, 4);
-
     }
-
+    public LocalDate getFechaLimite() {
+        return fechaLimite;
+    }
     public void setPrioridad(int prioridad) {
         this.prioridad = prioridad;
     }
-
-    public void setEstado(boolean estado) {
-        this.estado = estado;
+    public int getPrioridad(){
+        return this.prioridad;
     }
-    public boolean getEstado(){
+    public Estado getEstado(){
         return this.estado;
     }
-
-    public void mostrarTarea(){
-        if(this.getEstado()){
-            System.out.println(this.descripcion);
+//    FUNCIONES:
+    public void completarTarea(){
+        // Si el estado de la tarea NO es incompleta, no hace nada.
+        if (!(this.estado == Estado.incompleta)){
+            return;
         }
+
+        // Si el estado de la tarea es incompleta marca la fecha de completación.
+        this.fechaCompletada = LocalDate.now();
+
+        // Comprobamos si la tarea es vencida o no y le asignamos su estado correspondiente.
+        if(this.fechaLimite.isBefore(LocalDate.now())){
+            this.estado = Estado.vencida;
+            return;
+        }
+        this.estado = Estado.completa;
+    }
+    public void mostrarTarea(){
+//        System.out.println("Estado: " + getEstado() +".\n Tarea: " + getDescripcion() + ".\n Prioridad: " + getPrioridad());
+        System.out.println("Tarea: " + getDescripcion() + "\n Estado: " + getEstado() + ".\n Prioridad: " + getPrioridad());
+    }
+    public boolean tareaVencida(){
+        return this.estado == Estado.vencida;
     }
 }
