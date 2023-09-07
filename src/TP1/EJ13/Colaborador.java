1c2e0e6be692931536a2c91487f5924dc04d4503
Luis Mayor Moreno <lmayormoreno@gmail.com>
2023-09-07 19:07:13 -0300

    Trabajo Terminado

--- src/TP1/EJ13/Colaborador.java
+++ src/TP1/EJ13/Colaborador.java
@@ -1,5 +1,14 @@
 package TP1.EJ13;
 
 public class Colaborador {
+    private String nombre;
+    private String Apellido;
 
+    public Colaborador(String nombre){
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
 }
--- src/TP1/EJ13/TareaColaborador.java
+++ src/TP1/EJ13/TareaColaborador.java
@@ -9,7 +9,8 @@ import java.time.LocalDate;
 
 public class TareaColaborador extends Tarea{
     private String titulo;
-    LocalDate fechaRecordatorio;
+    private LocalDate fechaRecordatorio;
+    private Colaborador colaboradorTermindo = null;
     public TareaColaborador(String descripcion, String titulo, int prioridad) {
         this(descripcion, titulo, prioridad, null);
     }
@@ -37,6 +38,10 @@ public class TareaColaborador extends Tarea{
         this.titulo = titulo;
     }
 
+    public Colaborador getColaboradorTermindo() {
+        return colaboradorTermindo;
+    }
+
     @Override
     public void mostrarTarea() {
         revisarEstado();
@@ -47,6 +52,27 @@ public class TareaColaborador extends Tarea{
                            ".\n Fecha de recordatorio: " + (this.fechaRecordatorio == null ? " Sin definir " : this.fechaRecordatorio)
         );
     }
+    @Override
+    public void completarTarea(){
+        throw new UnsupportedOperationException("El método no está disponible sin que se le ingrese un colaborador");
+    }
+    public void completarTarea(Colaborador colaborador){
+        // Si el estado de la tarea NO es incompleta, no hace nada.
+        if ((this.estado == Estado.vencida || this.estado == Estado.completa)){
+            return;
+        }
+
+        // Si el estado de la tarea es incompleta marca la fecha de completación.
+        this.fechaCompletada = LocalDate.now();
+
+        // Comprobamos si la tarea es vencida o no y le asignamos su estado correspondiente.
+        if(this.fechaLimite.isBefore(LocalDate.now())){
+            this.estado = Estado.vencida;
+            return;
+        }
+        this.colaboradorTermindo = colaborador;
+        this.estado = Estado.completa;
+    }
 
     public String toString(){
 
@@ -56,7 +82,8 @@ public class TareaColaborador extends Tarea{
                 ".\n Estado: " + getEstado() +
                 ".\n Prioridad: " + getPrioridad() +
                 ".\n Fecha de vencimiento: " + (this.fechaLimite == null ? " Sin definir " : this.fechaLimite) +
-                ".\n Fecha de recordatorio: " + (this.fechaRecordatorio == null ? " Sin definir " : this.fechaRecordatorio);
+                ".\n Fecha de recordatorio: " + (this.fechaRecordatorio == null ? " Sin definir " : this.fechaRecordatorio) +
+                ".\n Terminado por: " + (this.getEstado() == Estado.completa ? this.colaboradorTermindo.getNombre() : "- Sin definir -");
     }
 
     private void revisarEstado(){
